bf813a602dd95d8603afe9f54aeaf2e4df349887
Andrew Halberstadt <ahal@pm.me>
2020-03-12 16:22:28 -0400

    Remove local scheduler support from scheduler_analysis recipe

--- recipes/scheduler_analysis.py
+++ recipes/scheduler_analysis.py
@@ -10,7 +10,6 @@ Run an analysis of all the scheduling algorithms defined in the
 import os
 import subprocess
 import sys
-from argparse import Namespace
 from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime
@@ -18,31 +17,16 @@ from pathlib import Path
 
 from adr import config
 from adr.errors import MissingDataError
-from adr.query import run_query
 from adr.util.memoize import memoized_property
-from icecream import ic
 from loguru import logger
 
-from mozci.push import Push, make_push_objects
+from mozci.push import make_push_objects
 
 here = Path(__file__).parent.resolve()
 
 
 RUN_CONTEXTS = [
     {
-        "gecko_path": {
-            "flags": ["--gecko-path"],
-            "dest": "gecko_path",
-            "default": os.environ.get("GECKO_PATH"),
-            "help": "Path to gecko (i.e mozilla-central).",
-        },
-        "clone": {
-            "flags": ["--clone"],
-            "dest": "clone",
-            "action": "store_true",
-            "default": False,
-            "help": "Clone Gecko if the specified path does not exist.",
-        },
         "strategies": {
             "nargs": "+",
             "default": [],
@@ -50,8 +34,6 @@ RUN_CONTEXTS = [
         }
     }
 ]
-
-GECKO = None
 
 
 @dataclass
@@ -93,21 +75,8 @@ class Scheduler:
         self.name = name
         self.score = Score()
 
-    @memoized_property
-    def path(self):
-        # initialize schedulers to analyze
-        cwd = os.getcwd()
-        strategy_dir = here / "strategies"
-        for s in strategy_dir.glob("*.py"):
-            if s.stem == self.name:
-                return s
-
     def get_tasks(self, push):
         key = f"scheduler.{push.rev}.{self.name}"
-        if self.path:
-            with open(self.path, "r") as fh:
-                scheduler_hash = config.cache._hash(fh.read())
-            key += f".{scheduler_hash}"
 
         if config.cache.has(key):
             logger.opt(ansi=True).info(f"<cyan>{self.name} loaded from cache</cyan>")
@@ -127,28 +96,8 @@ class Scheduler:
             config.cache.put(key, tasks, 43200)  # keep results for 30 days
             return tasks
 
-        # Finally fallback to generating the tasks locally.
-        if not GECKO or not self.path:
-            logger.warning(f"warning: shadow scheduler '{self.name}' not found!")
-            raise MissingDataError
-
-        logger.info(f"Generating target tasks")
-        cmd = ["./mach", "taskgraph", "optimized", "--fast"]
-        env = os.environ.copy()
-        env.update(
-            {
-                "PYTHONPATH": self.path.parent.as_posix(),
-                "TASKGRAPH_OPTIMIZE_STRATEGIES": f"{self.name}:STRATEGIES",
-            }
-        )
-        output = subprocess.check_output(
-            cmd, env=env, cwd=GECKO, stderr=subprocess.DEVNULL
-        ).decode("utf8")
-        tasks = set(output.splitlines())
-
-        logger.opt(ansi=True).info(f"<cyan>{self.name} loaded from local repository</cyan>")
-        config.cache.put(key, tasks, 43200)  # keep results for 30 days
-        return tasks
+        logger.warning(f"warning: shadow scheduler '{self.name}' not found!")
+        raise MissingDataError
 
     def analyze(self, push):
         tasks = self.get_tasks(push)
@@ -165,30 +114,7 @@ class Scheduler:
         return score
 
 
-def hg(args):
-    cmd = ["hg"] + args
-    logger.debug(f"Running: {' '.join(cmd)}")
-    return subprocess.check_output(cmd, cwd=GECKO).decode("utf8")
-
-
-def clone_gecko():
-    cmd = ['hg', 'clone', 'https://hg.mozilla.org/mozilla-unified', GECKO]
-    logger.debug(f"Running: {' '.join(cmd)}")
-    subprocess.call(cmd)
-
-
 def run(args):
-    global GECKO, logger
-    if args.gecko_path:
-        GECKO = args.gecko_path
-
-    if GECKO and not Path(GECKO).is_dir():
-        if args.clone:
-            clone_gecko()
-        else:
-            logger.error(f"Gecko path '{GECKO}' does not exist! Pass --clone to clone it to this location.")
-            sys.exit(1)
-
     schedulers = []
     for s in args.strategies:
         logger.info(f"Creating scheduler using strategy {s}")
@@ -209,49 +135,35 @@ def run(args):
         date = datetime.utcfromtimestamp(push.date).strftime('%Y-%m-%d')
         pushes_by_date[date].append(push)
 
-    if GECKO:
-        orig_rev = hg(["log", "-r", ".", "-T", "{node}"])
-        logger.info(f"Found previous revision: {orig_rev}")
+    i = 0
+    for date in sorted(pushes_by_date):
+        pushes = pushes_by_date[date]
+        logger.info(f"Analyzing pushes from {date} ({len(pushes)} pushes)")
 
-    try:
-        i = 0
-        for date in sorted(pushes_by_date):
-            pushes = pushes_by_date[date]
-            logger.info(f"Analyzing pushes from {date} ({len(pushes)} pushes)")
+        _hash = config.cache._hash(''.join([p.rev for p in pushes]) +
+                                   ''.join([s.name for s in schedulers]))
+        key = f"scheduler_analysis.{date}.{_hash}"
+        if config.cache.has(key):
+            logger.info(f"Loading results for {date} from cache")
+            data = config.cache.get(key)
 
-            _hash = config.cache._hash(''.join([p.rev for p in pushes]) +
-                                       ''.join([s.name for s in schedulers]))
-            key = f"scheduler_analysis.{date}.{_hash}"
-            if config.cache.has(key):
-                logger.info(f"Loading results for {date} from cache")
-                data = config.cache.get(key)
+            for s in schedulers:
+                s.score.update(Score(**data[s.name]))
+            i += len(pushes)
+            continue
 
-                for s in schedulers:
-                    s.score.update(Score(**data[s.name]))
-                i += len(pushes)
-                continue
+        scores = defaultdict(Score)
+        for push in sorted(pushes, key=lambda p: p.id):
+            i += 1
+            logger.info(f"Analyzing https://treeherder.mozilla.org/#/jobs?repo=autoland&revision={push.rev} ({i}/{total_pushes})")  # noqa
 
-            scores = defaultdict(Score)
-            for push in sorted(pushes, key=lambda p: p.id):
-                i += 1
-                logger.info(f"Analyzing https://treeherder.mozilla.org/#/jobs?repo=autoland&revision={push.rev} ({i}/{total_pushes})")  # noqa
+            for s in schedulers:
+                try:
+                    scores[s.name].update(s.analyze(push))
+                except MissingDataError:
+                    logger.warning(f"MissingDataError: Skipping {push.rev}")
 
-                if GECKO:
-                    hg(["update", push.rev])
-
-                for s in schedulers:
-                    try:
-                        scores[s.name].update(s.analyze(push))
-                    except MissingDataError:
-                        logger.warning(f"MissingDataError: Skipping {push.rev}")
-
-            config.cache.put(key, {k: v.as_dict() for k, v in scores.items()}, 43200)  # 30 days
-
-
-    finally:
-        if GECKO:
-            logger.info("restoring repo")
-            hg(["update", orig_rev])
+        config.cache.put(key, {k: v.as_dict() for k, v in scores.items()}, 43200)  # 30 days
 
     header = [
         "Scheduler",
